dcbfaa577fce0d79d7d85858c3ed931ba8d26a49
C0LDRAM3N <emersonwyatt@outlook.com>
2026-02-05 19:02:54 -0800

    More plugins + Customization

--- lua/custom/plugins/kanagawa.lua
+++ lua/custom/plugins/kanagawa.lua
@@ -20,9 +20,9 @@ return {
       overrides = function(colors) -- add/modify highlights
         return {}
       end,
-      theme = 'wave', -- Load "wave" theme
+      theme = 'dragon', -- Load "wave" theme
       background = { -- map the value of 'background' option to a theme
-        dark = 'wave', -- try "dragon" !
+        dark = 'dragon', -- try "dragon" !
         light = 'lotus',
       },
     }
